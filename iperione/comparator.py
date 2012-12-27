719adfb312d4db09629323e9f851fc7f333236da
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2012-12-27 10:04:42 +0100

    [comparator] created

---
+++ iperione/comparator.py
@@ -0,0 +1,7 @@
+
+class Comparator:
+	def setFile1(self, url):
+		self.file1 = url
+
+	def setFile2(self, url):
+		self.file2 = url
---
+++ test/test_comparator.py
@@ -0,0 +1,15 @@
+import sys,os
+sys.path.insert(0,os.path.abspath(__file__+"/../.."))
+
+from iperione.comparator import Comparator
+import unittest
+
+class Test_Comparator(unittest.TestCase):
+	def setUp(self):
+		self.comparator = Comparator()
+
+	def test_set_no_existent_file(self):
+		self.assertRaises(NameError, self.comparator.setFile1, ("noExistent"))
+		
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_Comparator)
+unittest.TextTestRunner(verbosity=2).run(suite)
