92f1c4a0b5c79c60763997918a1ca76fa457ff88
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-01-01 16:47:08 +0100

    [test] path to content resolved correctly

--- test/test_comparator.py
+++ test/test_comparator.py
@@ -7,18 +7,19 @@ import unittest
 class Test_Comparator(unittest.TestCase):
 	def setUp(self):
 		self.comparator = Comparator()
+		self.path = os.path.dirname(__file__)
 
 	def test_set_no_existent_file(self):
 		self.assertRaises(IOError, self.comparator.setExpected, ("noExistent"))
 		self.assertRaises(IOError, self.comparator.setObtained, ("noExistent"))
 
 	def test_set_existent_file(self):
-		self.comparator.setExpected("dummy.wav")
-		self.comparator.setObtained("dummy.wav")
+		self.comparator.setExpected(self.path + "/dummy.wav")
+		self.comparator.setObtained(self.path + "/dummy.wav")
 
 	def test_compare_two_file(self):
-		self.comparator.setExpected("dummy.wav")
-		self.comparator.setObtained("dummy.wav")
+		self.comparator.setExpected(self.path + "/dummy.wav")
+		self.comparator.setObtained(self.path + "/dummy.wav")
 		self.assertTrue(self.comparator.areEqual())
 		
 suite = unittest.TestLoader().loadTestsFromTestCase(Test_Comparator)
