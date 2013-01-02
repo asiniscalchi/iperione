94280d2418996f2ea9062b66cfb46a0373802b51
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-01-02 22:57:35 +0100

    [Comparator_wav] created with tests

---
+++ iperione/comparator_wav.py
@@ -0,0 +1,14 @@
+import os, sys, difflib
+from comparator import Comparator
+
+class Comparator_wav(Comparator):
+	def _specializedCompare(self):
+		return
+#		expectedLines = open(self.expected, 'U').readlines()
+#		resultLines = open(self.result, 'U').readlines()
+#		self.diff = difflib.unified_diff(expectedLines, resultLines, self.expected, self.result)
+
+	def areEqual(self):
+		return True		
+	
+
---
+++ test/test_comparator_wav.py
@@ -0,0 +1,23 @@
+import sys,os
+sys.path.insert(0,os.path.abspath(__file__+"/../.."))
+
+from iperione.comparator_wav import Comparator_wav
+import unittest
+
+class Test_Comparator_wav(unittest.TestCase):
+	def setUp(self):
+		self.comparator = Comparator_wav()
+		self.contentPath = os.path.dirname(__file__) + "/content"
+
+	def test_equal_files(self):
+		self.comparator.setExpected(self.contentPath + "/dummy.wav")
+		self.comparator.setResult(self.contentPath + "/dummy.wav")
+		self.assertTrue(self.comparator.areEqual())
+
+	def test_different_files(self):
+		self.comparator.setExpected(self.contentPath + "/dummy.wav")
+		self.comparator.setResult(self.contentPath + "/dummy2.wav")
+                self.assertFalse(self.comparator.areEqual())
+		
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_Comparator_wav)
+unittest.TextTestRunner(verbosity=2).run(suite)
