aa249a90953cd4be126cfcafa7a2ad5751d81578
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-01-04 10:34:12 +0100

    [compare_audio] added diff_nSamples

--- iperione/comparator_audio.py
+++ iperione/comparator_audio.py
@@ -5,18 +5,15 @@ from comparator import Comparator
 from scikits.audiolab import Sndfile
 
 class Comparator_audio(Comparator):
+	def __init__(self):
+		self.areEqual = True
+		self.diff_nSamples = 0
+
 	def _specializedCompare(self):
+		self.__init__()
 		self.sndfile_expected = Sndfile(self.expected)
 		self.sndfile_result = Sndfile(self.result)
 		if self.sndfile_result.nframes != self.sndfile_expected.nframes:
-			self.diff = "ciaociao"
-		else:
-			self.diff = None
-	
-	def areEqual(self):
-		if self.diff != None:
-			return False
-
-		return True		
-	
+			self.areEqual = False
+			self.diff_nSamples = self.sndfile_result.nframes - self.sndfile_expected.nframes
 
--- test/test_comparator_audio.py
+++ test/test_comparator_audio.py
@@ -12,12 +12,13 @@ class Test_Comparator_audio(unittest.TestCase):
 	def test_same_file(self):
 		self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
 		self.comparator.setResult(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
-		self.assertTrue(self.comparator.areEqual())
+		self.assertTrue(self.comparator.areEqual)
 
 	def test_different_duration_files(self):
 		self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
 		self.comparator.setResult(self.contentPath + "/sine_440Hz_2sec_44100_16bits.wav")
-                self.assertFalse(self.comparator.areEqual())
+                self.assertFalse(self.comparator.areEqual)
+		self.assertEqual(self.comparator.diff_nSamples, 44100)
 		
-suite = unittest.TestLoader().loadTestsFromTestCase(Test_Comparator_wav)
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_Comparator_audio)
 unittest.TextTestRunner(verbosity=2).run(suite)
