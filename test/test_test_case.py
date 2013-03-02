4d05248477e671dafaebc9ae966aabb4ab9ead3e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-03-02 11:16:47 +0100

    [test] using temporary files

--- test/test_test_case.py
+++ test/test_test_case.py
@@ -1,4 +1,4 @@
-import sys, os, subprocess 
+import sys, os, subprocess, tempfile 
 import iperione
     
 class Test_TestCase(iperione.TestCase):
@@ -18,9 +18,10 @@ class Test_TestCase(iperione.TestCase):
 	self.assertRaises(AssertionError,  self.assertAudioFileEqual,  "sine_440Hz_1sec_44100_16bits.wav",  "square_440Hz_1sec_44100_16bits.wav")
 
     def test_copied_file(self):
-	command = [sys.executable, os.path.join(self.commandPath, "copyfile.py"), os.path.join(self.inputPath, "sine_440Hz_1sec_44100_16bits.wav"), os.path.join(self.resultPath, "sine_440Hz_1sec_44100_16bits_copy.wav")]
+	result = tempfile.NamedTemporaryFile().name
+	command = [sys.executable, os.path.join(self.commandPath, "copyfile.py"), os.path.join(self.inputPath, "sine_440Hz_1sec_44100_16bits.wav"), os.path.join(self.resultPath, result)]
         self.execute(command)
-        self.assertAudioFileEqual("sine_440Hz_1sec_44100_16bits.wav",  "sine_440Hz_1sec_44100_16bits_copy.wav")
+        self.assertAudioFileEqual("sine_440Hz_1sec_44100_16bits.wav",  result)
 
 if __name__ == "__main__":
     iperione.main()
