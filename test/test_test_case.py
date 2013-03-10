e8904095af506ad0acbdd9b8a805707270e5b139
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-03-10 17:36:22 +0100

    [Test_TestCase] removed test, better names
    Signed-off-by: Alessandro Siniscalchi <asiniscalchi@gmail.com>

--- test/test_test_case.py
+++ test/test_test_case.py
@@ -8,21 +8,14 @@ class Test_TestCase(iperione.TestCase):
         self.inputPath = os.path.join(os.path.dirname(__file__), 'content')
         self.commandPath = os.path.join(os.path.dirname(__file__), 'content')
         
-    def test_same_files(self):
+    def test_audio_same_files(self):
         self.assertAudioFileEqual("sine_440Hz_1sec_44100_16bits.wav",  "sine_440Hz_1sec_44100_16bits.wav")
 
-    def test_different_files_duration(self):
+    def test_audio_different_files_duration(self):
         self.assertRaises(AssertionError,  self.assertAudioFileEqual,  "sine_440Hz_1sec_44100_16bits.wav",  "sine_440Hz_2sec_44100_16bits.wav")
 
-    def test_different_audio_file(self):
+    def test_audio_different_audio_file(self):
         self.assertRaises(AssertionError,  self.assertAudioFileEqual,  "sine_440Hz_1sec_44100_16bits.wav",  "square_440Hz_1sec_44100_16bits.wav")
-
-    def test_copied_audio_file(self):
-        result = iperione.getUniqueFilename()
-        command = [sys.executable, os.path.join(self.commandPath, "copyfile.py"), os.path.join(self.inputPath, "sine_440Hz_1sec_44100_16bits.wav"), os.path.join(self.getResultPath(), result)]
-        iperione.execute(command, shell=False)
-        self.assertAudioFileEqual("sine_440Hz_1sec_44100_16bits.wav",  result)
-        os.remove(os.path.join(self.getResultPath(), result))
         
     def test_txt_same_file(self):
         self.assertTxtFileEqual("dummy.txt",  "dummy.txt")
