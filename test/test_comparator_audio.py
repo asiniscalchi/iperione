a7cd2a52c6827657bb40d09ccaff78c1abb81947
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-03-10 17:37:35 +0100

    [identation]
    Signed-off-by: Alessandro Siniscalchi <asiniscalchi@gmail.com>

--- test/test_comparator_audio.py
+++ test/test_comparator_audio.py
@@ -3,21 +3,21 @@ import iperione
 from iperione.comparator_audio import ComparatorAudio
 
 class Test_Comparator_audio(iperione.TestCase):
-	def setUp(self):
-		self.comparator = ComparatorAudio()
-		self.contentPath = os.path.dirname(__file__) + "/content"
+    def setUp(self):
+        self.comparator = ComparatorAudio()
+        self.contentPath = os.path.dirname(__file__) + "/content"
 
-	def test_same_file(self):
-		self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
-		self.comparator.setResult(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
-		self.comparator.run()
-		self.assertTrue(self.comparator.diff == None)
+    def test_same_file(self):
+        self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
+        self.comparator.setResult(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
+        self.comparator.run()
+        self.assertTrue(self.comparator.diff == None)
 
-	def test_different_duration_files(self):
-		self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
-		self.comparator.setResult(self.contentPath + "/sine_440Hz_2sec_44100_16bits.wav")
-		self.comparator.run()
-                self.assertFalse(self.comparator == None)
+    def test_different_duration_files(self):
+        self.comparator.setExpected(self.contentPath + "/sine_440Hz_1sec_44100_16bits.wav")
+        self.comparator.setResult(self.contentPath + "/sine_440Hz_2sec_44100_16bits.wav")
+        self.comparator.run()
+        self.assertFalse(self.comparator == None)
 
 if __name__ == "__main__":
     iperione.main()
