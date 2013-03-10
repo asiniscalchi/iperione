47a61cded6f919427e8ab5af29d041d8dd7c42e2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2013-03-10 17:57:28 +0100

    [ideantation]

--- test/test_comparator.py
+++ test/test_comparator.py
@@ -5,17 +5,17 @@ class Test_Comparator(object):
     def __init__(self):
         self.__test__ = False
         
-	def setUp(self):
-		self.comparator = Comparator()
-		self.contentPath = os.path.dirname(__file__) + "/content"
+    def setUp(self):
+        self.comparator = Comparator()
+        self.contentPath = os.path.dirname(__file__) + "/content"
 
-	def test_set_no_existent_file(self):
-		self.assertRaises(IOError, self.comparator.setExpected, ("noExistent"))
-		self.assertRaises(IOError, self.comparator.setResult, ("noExistent"))
+    def test_set_no_existent_file(self):
+        self.assertRaises(IOError, self.comparator.setExpected, ("noExistent"))
+        self.assertRaises(IOError, self.comparator.setResult, ("noExistent"))
 
-	def test_set_existent_file(self):
-		self.comparator.setExpected(self.contentPath + "/dummy.wav")
-		self.comparator.setResult(self.contentPath + "/dummy.wav")
-	
+    def test_set_existent_file(self):
+        self.comparator.setExpected(self.contentPath + "/dummy.wav")
+        self.comparator.setResult(self.contentPath + "/dummy.wav")
+    
 if __name__ == "__main__":
     iperione.main()
